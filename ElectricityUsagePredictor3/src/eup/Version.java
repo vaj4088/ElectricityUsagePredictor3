f72ae55510709d4e1c7701fab4a8ed36cff4f4f8
vaj4088 <ianshef@yahoo.com>
2018-07-29 20:20:54 -0500

    Snapshot 20180729 towards a working program.

--- ElectricityUsagePredictor3/src/eup/Version.java
+++ ElectricityUsagePredictor3/src/eup/Version.java
@@ -19,8 +19,8 @@ public final class Version<T> implements Serializable, Comparable<T> {
      *  
      */
     private static final String VERSION = "Revised on"
-	    + " Date: 2018-07-21"
-	    + " at 1726"
+	    + " Date: 2018-07-29"
+	    + " at 2019"
 	    + " via manual intervention.";
     private static final long serialVersionUID = 1L;
     private static final Version<String> INSTANCE = new Version<String>();
